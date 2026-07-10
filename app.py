c5d4ae63a91cbdc9f8bdc28fa3abfec1292a253c
bhavi0906 <104507702+bhavi0906@users.noreply.github.com>
2026-07-10 18:07:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,43 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
+# Secret key used for sessions and forms
+app.config["SECRET_KEY"] = "cloudsecurityproject"
+
+# Create database folder if it doesn't exist
+os.makedirs("database", exist_ok=True)
+
+# SQLite database path
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/users.db"
+
+# Disable modification tracking
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Initialize database
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    username = db.Column(db.String(100), unique=True, nullable=False)
+
+    email = db.Column(db.String(150), unique=True, nullable=False)
+
+    password = db.Column(db.String(255), nullable=False)
+
+
 @app.route("/")
 def home():
-    return """
-    <h1>Cloud Security Demo</h1>
-    <p>Welcome to my Azure hosted Flask application.</p>
-    """
+    return "<h1>Cloud Security Dashboard</h1>"
+
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
