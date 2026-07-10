75809f0d8b8fc055a16952ed34f9149599ade64e
bhavi0906 <bhavs329@gmail.com>
2026-07-10 17:19:07 +0530

    initial Flask App

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Cloud Security Demo</h1>
+    <p>Welcome to my Azure hosted Flask application.</p>
+    """
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
