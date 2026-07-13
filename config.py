737127514dde1bf82c0a98ca4bae9b86058caf00
bhavi0906 <bhavs329@gmail.com>
2026-07-13 17:35:07 +0530

    setting azure sql

--- config.py
+++ config.py
@@ -1,8 +1,40 @@
 import os
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+from urllib.parse import quote_plus
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
     SECRET_KEY = "cloudsecurityproject"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "database", "users.db")
+
+    # Local SQLite (fallback)
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
+        basedir, "database", "users.db"
+    )
+
+    # Running on Azure?
+    if os.getenv("WEBSITE_SITE_NAME"):
+
+        vault_url = "https://bhavyansh-keyvault.vault.azure.net/"
+
+        credential = DefaultAzureCredential()
+
+        client = SecretClient(
+            vault_url=vault_url,
+            credential=credential
+        )
+
+        conn = client.get_secret(
+            "AZURE-SQL-CONNECTION-STRING"
+        ).value
+
+        
+        
+
+        SQLALCHEMY_DATABASE_URI = (
+            "mssql+pyodbc:///?odbc_connect=" + quote_plus(conn)
+        )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
