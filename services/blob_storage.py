533bb4ce754bd1cc5c7ac323987c32d83a07659a
bhavi0906 <bhavs329@gmail.com>
2026-07-13 16:17:05 +0530

    integrated azure key vault

--- services/blob_storage.py
+++ services/blob_storage.py
@@ -1,12 +1,36 @@
-# deployment tetst - july 13
 from azure.storage.blob import BlobServiceClient
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 from werkzeug.utils import secure_filename
 import os
 
+# Load .env only for local development
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
+
+# First try local .env
 CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+
+# If not found, fetch from Azure Key Vault
+if not CONNECTION_STRING:
+    KEYVAULT_NAME = "bhavyansh-keyvault"
+    KV_URI = f"https://{KEYVAULT_NAME}.vault.azure.net/"
+
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url=KV_URI, credential=credential)
+
+    CONNECTION_STRING = client.get_secret(
+        "AZURE-STORAGE-CONNECTION-STRING"
+    ).value
+
 CONTAINER_NAME = "uploads"
 
-blob_service_client = BlobServiceClient.from_connection_string(CONNECTION_STRING)
+blob_service_client = BlobServiceClient.from_connection_string(
+    CONNECTION_STRING
+)
 
 def upload_file(file):
     filename = secure_filename(file.filename)
